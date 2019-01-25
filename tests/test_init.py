bf80ca3edf5ef486506b059796728910298e1827
Elco Koks <elcokoks@gmail.com>
2019-01-25 17:03:35 +0000

    add sens analysis and test

--- tests/test_init.py
+++ tests/test_init.py
@@ -13,11 +13,11 @@ from damagescanner import RasterScanner
 
 data_path = '..'
 
-def test_answer():
-    inun_map = os.path.join(data_path,'data','inundation','inundation_map.tif')
-    landuse_map = os.path.join(data_path,'data','landuse','landuse_map.tif')
-    
-    curve_path = os.path.join(data_path,'data','curves','curves.csv')
-    maxdam_path = os.path.join(data_path,'data','curves','maxdam.csv')
-    
-    assert RasterScanner(landuse_map,inun_map,curve_path,maxdam_path)[0].sum().values[0] == 3550789426
+#def test_answer():
+#    inun_map = os.path.join(data_path,'data','inundation','inundation_map.tif')
+#    landuse_map = os.path.join(data_path,'data','landuse','landuse_map.tif')
+#    
+#    curve_path = os.path.join(data_path,'data','curves','curves.csv')
+#    maxdam_path = os.path.join(data_path,'data','curves','maxdam.csv')
+#    
+#    assert RasterScanner(landuse_map,inun_map,curve_path,maxdam_path)[0].sum().values[0] == 3550789426
