efa03b69b9df4bb3762acc02a57185fb7a399bcb
Koks <elco.koks@vu.nl>
2024-01-26 16:03:56 +0100

    example notebooks

--- src/damagescanner/core.py
+++ src/damagescanner/core.py
@@ -8,7 +8,7 @@ import shapely
 import pandas as pd
 import geopandas as gpd
 from tqdm import tqdm
-from pathlib import Path,PurePath
+from pathlib import Path
 import osm_flex.extract as ex
 
 from vector import VectorScanner
@@ -21,8 +21,8 @@ class DamageScanner(object):
         """Prepare the input for a damage assessment
         """
 
-        ## set up the assessment type and data path
         self.assessment_type = assessment_type
+        
         self.data_path = data_path       
 
         # Collect exposure data
@@ -53,24 +53,23 @@ class DamageScanner(object):
         if len(curves) > 0:
             self.curves = curves
         else:
-            self.curves = list(p.resolve() for p in Path(data_path / 'vulnerability').glob("**/*") if 'curve' in str(p))[0]
+            self.curves = list(p.resolve() for p in Path(data_path / 'vulnerability').glob("**/*") if 'curve' in str(p).lower())[0]
 
         # Collect maxdam information
         if len(maxdam) > 0:
             self.maxdam = maxdam
         else:
-            self.maxdam = list(p.resolve() for p in Path(data_path / 'vulnerability').glob("**/*") if 'dam' in str(p))[0]
+            self.maxdam = list(p.resolve() for p in Path(data_path / 'vulnerability').glob("**/*") if (('dam' in str(p).lower()) |
+                                                                                                       ('cost' in str(p).lower()))) [0]
        
         
     def single_event(self,hazard_type='',hazard_file='',return_period='',save_output=False,vector_landuse=True,**kwargs):
         """Damage assessment for a single event. Can be a specific hazard event, or a specific single hazard footprint 
         """
 
-        # check if the assessment type is set up
         if not hasattr(self, 'assessment_type'):
-            raise ImportError('ERROR: Please run .prepare() first to set up the core of the assessment')
+            raise ImportError('ERROR: Please run .prepare() first to set up the assessment')
 
-        # check if the hazard data is ready and available
         if hazard_file != '':
             self.hazard_map = [x for x in self.hazard_data if hazard_file in str(x)][0]
         else:      
@@ -81,7 +80,7 @@ class DamageScanner(object):
                     raise RuntimeError('Multiple events require the .multiple_events() function!')     
             elif hazard_type != '':
                 if hazard_type == 'flood':
-                    hazard_characteristics = ['inun','flood','fluvial','pluvial','coastal']
+                    hazard_characteristics = ['inun','flood','fluvial','pluvial','coastal','fd','pd']
                 elif hazard_type == 'windstorm':
                     hazard_characteristics = ['storm','wind','cyclone','tc']
                 elif hazard_type == 'earthquake':
@@ -89,10 +88,10 @@ class DamageScanner(object):
                 elif hazard_type == 'landslide':
                     hazard_characteristics = ['ls','landslide']
                 
-                hazard_maps = [x for x in self.hazard_data if any(y in str(x) for y in hazard_characteristics)]
+                hazard_maps = [x for x in self.hazard_data if any(y in str(x).lower() for y in hazard_characteristics)]
                 
                 if return_period != '':
-                    self.hazard_map = [x for x in hazard_maps if return_period in str(x)][0]
+                    self.hazard_map = [x for x in hazard_maps if return_period in str(x)].lower()[0]
                 
                 elif return_period == '':
                     self.hazard_map = hazard_maps[0]
@@ -216,6 +215,9 @@ class DamageScanner(object):
         """
         """
         extracted_exposure = ex.extract_cis(self.exposure_data, infra_type)
+
+        if infra_type == 'road':
+            extracted_exposure = extracted_exposure.loc[extracted_exposure.geometry.geom_type == 'LineString']
         
         extracted_exposure.geometry = shapely.make_valid(extracted_exposure.geometry)        
 
