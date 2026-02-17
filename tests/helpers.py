26e66b7fbfe4001071d5ce89d1bb58ef802022a0
Jens de Bruijn <2176353+jensdebruijn@users.noreply.github.com>
2026-02-17 08:19:13 +0100

    fix ruff format

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,12 +4,12 @@ data_path = pathlib.Path(__file__).parent.parent / "data"
 output_folder = pathlib.Path("tests/output")
 tmp_folder = pathlib.Path("tests/tmp")
 
+
 def create_test_folders():
     """Create output and tmp folders for tests if they don't exist."""
     output_folder.mkdir(exist_ok=True)
     tmp_folder.mkdir(exist_ok=True)
 
+
 # Run once on import
 create_test_folders()
-
-
